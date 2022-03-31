804bde01028697112b7325c88ec48911a23e246a
GTNT-Github <graemeiburtis@gmail.com>
2022-03-31 17:12:12 -0400

    fixing stuff

--- assets/fight_tables.lua
+++ assets/fight_tables.lua
@@ -1,6 +1,6 @@
 -- Fight Enemy
 approach = {["Guard1"] = 2, ["Guard2"] = 12}
-attack = {["Guard1"] = 10, ["Guard2"] = 15 }
+attack = {["Guard1"] = 10, ["Guard2"] = 15}
 stats = {["Guard1"] = 3, ["Guard2"] = 13}
 text_2 = {["Guard1"] = 4, ["Guard2"] = 14}
 text_2_options = {["Guard1"] = math.random(5,6), ["Guard2"] = math.random(15,16)}
@@ -28,7 +28,7 @@ collisions = {["Guard1"] = "Guard1", ["Guard2"] = "Guard2"}
 collision_teleport = {["Guard1"] = "Guard1Teleport", ["Guard2"] = "Guard2Teleport"}
 
 --Camera
-camera_pos = {["Guard1"] = vmath.vector3(972,610,1),["Guard2"] = vmath.vector3(2530,386,1)}
+camera_pos = {["Guard1"] = vmath.vector3(972,610,1),["Guard2"] = vmath.vector3(2530,390,1)}
 can_flee = {["Guard1"] = false, ["Guard2"] = false}
 original_pos = {["Guard1"] = vmath.vector3(971,905, 1), ["Guard2"] = vmath.vector3(2338.55,484.5,1)}
-camera_zoom = {["Guard1"] = 0.7, ["Guard2"] = 0.7}
+camera_zoom = {["Guard1"] = 0.7, ["Guard2"] = 0.6}
